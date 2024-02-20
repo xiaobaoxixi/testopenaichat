4dc464ad41d72079c185a6971206165afd9cfc49
xiaobaoxixi <liyan.xiaobaoxixi@gmail.com>
2024-02-20 18:48:28 +0100

    changed assistant instruction

--- app/api/assistant/create/route.ts
+++ app/api/assistant/create/route.ts
@@ -6,12 +6,12 @@ export async function GET() {
   try {
     const assistant = await openai.beta.assistants.create({
       instructions: `
-      You are a professional stock analyst. 
-      I will ask you questions about the stock market and you will answer them.
+      You are a data analyst. 
+      I will ask you to interpret dataset and you will answer them.
       You can use the documents I provide to you to help you answer the questions.
       If you're not 100% sure of the answer, you can say "I don't know".
         `,
-      name: "Mini Stock Analyst",
+      name: "Mini Analyst",
       tools: [{ type: "retrieval" }],
       model: "gpt-4-1106-preview",
     });
